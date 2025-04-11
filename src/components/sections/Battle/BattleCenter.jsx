90bf091c87c2b3fa4ce6cb1480eac1f23fbb9e25
8890aman <95869093+8890aman@users.noreply.github.com>
2025-04-12 00:53:20 +0530

    Revert: Remove animated GIF from BattleCenter background

--- src/components/sections/Battle/BattleCenter.jsx
+++ src/components/sections/Battle/BattleCenter.jsx
@@ -88,7 +88,7 @@ const BattleCenter = () => {
           {/* Background will be replaced with an image */}
           <div className="absolute inset-0 bg-cover bg-center opacity-25"
                style={{ 
-                 backgroundImage: `url('https://i.pinimg.com/originals/7e/30/f6/7e30f6211a5f6964c45b1a7d222fff45.gif')`,
+                 backgroundImage: `url('/battle-arena-placeholder.png')`,
                  backgroundSize: 'cover',
                  backgroundPosition: 'center',
                  /* 
